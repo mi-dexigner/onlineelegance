a493cf54d734507530f0bc1da34d6fb526da63f0
Idris <midexigner@gmail.com>
2024-05-07 09:34:39 +0500

    one more issue fixed again

--- components/HeroSlide.tsx
+++ components/HeroSlide.tsx
@@ -2,7 +2,7 @@ import Media from "./Media";
 
 const HeroSlide = () => {
     return ( <section className="Hero-Slide">
-<Media src="https://via.placeholder.com/1200x400" fallbackSrc="https://via.placeholder.com/1200x400" alt="Slide" />
+<Media src="https://via.placeholder.com/1200x400" width={1200} height={400} alt="Slide" />
 <div className="caption">
     <h1>Elegance Store</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
