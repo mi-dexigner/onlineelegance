8e1e3502dfc6f58cdc607b3405cce3fd4c2e3167
Idris <midexigner@gmail.com>
2024-05-09 11:07:39 +0500

    webp enable

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    webp: true,
+    formats:['image/avif','image/webp'],
     unoptimized: false,
     domains: ["cdn.sanity.io","via.placeholder.com","fakestoreapi.com"],
     contentSecurityPolicy: "default-src 'self'; script-src 'none';",
